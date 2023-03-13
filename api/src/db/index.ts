4d55f46f2a93075daae2342705bddfd5aa13b60e
maxi326519 <97068314+maxi326519@users.noreply.github.com>
2023-03-13 10:33:47 -0300

    Update (.env) Add DB_NAME

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -5,12 +5,12 @@ const path = require('path');
 const { DataTypes } = require('sequelize');
 
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-console.log(DB_USER, DB_PASSWORD, DB_HOST);
+console.log(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME);
 
-const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/stock`, {
+const sequelize = new Sequelize(`mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
